e1d02dc593962726a81b542885576cb9a76e3222
colecody27 <colecody27@gmail.com>
2023-11-09 13:53:46 -0800

    Replace python files with better FTP implementation

---
+++ referenceFiles/pythonclient.py
@@ -0,0 +1,26 @@
+# Client code 
+import socket
+
+# Name and port number of the server to which want to connect
+#ip = socket.gethostbyname()
+serverName = "localhost"
+serverPort = 12000
+
+# Create a socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# Connect to the server
+clientSocket.connect((serverName, serverPort))
+
+# A string we want to send to the server
+data = "Hello World This is a very long string."
+
+bytesSent = 0
+
+# Keep sending bytes until all bytes are sent
+while bytesSent != len(data) :
+    # Send that string !
+    bytesSent += clientSocket.send(data[bytesSent :].encode())
+
+# Close the socket
+clientSocket.close()
---
+++ referenceFiles/pythonserv.py
@@ -0,0 +1,38 @@
+# Server code
+import socket
+
+ # The port on which to listen
+serverPort = 12000
+
+ # Create a TCP's socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ # Bind the socket to the port
+serverSocket.bind(('', serverPort))
+
+ # Start listening for incoming connections
+serverSocket.listen(1) 
+print ("The server is ready to receive")
+
+ # Forever accept incoming connections
+while True :
+    # Accept a connection ; get client's socket
+    connectionSocket, addr = serverSocket.accept()
+
+    # The temporary buffer
+    tmpBuff = "" 
+    data = ""
+    while len(data) != 40 :
+        # Receive whatever the newly connected client has to send
+        tmpBuff = connectionSocket.recv(40).decode()
+
+        # The other side unexpectedly closed it’s socket
+        if not tmpBuff :
+            break
+
+        # Save the data
+        data += tmpBuff
+        print (data)
+
+    # Close the socket
+    connectionSocket.close()
